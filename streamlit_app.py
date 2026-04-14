64995333e30ca30176378de44cd0c7b11369f519
Nasir625 <prof.anraza3@gmail.com>
2026-04-14 20:50:44 +0530

    Add emoji to the title in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("SMN_AI")
+st.title("SMN_AI🤖")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
