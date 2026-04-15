2283e6032567e33b8d71695de84b6c7b57dca517
Nasir625 <prof.anraza3@gmail.com>
2026-04-15 06:19:20 +0530

    Fix syntax error in button condition

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 user_question = st.text_input
-if st.button("Submit")-
+if st.button("Submit"):
     st.title("SMN_AI🤖")
     st.write(
         "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
