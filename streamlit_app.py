c93b7ef500b90044ae0363bbc24c5b4d48afa161
Nasir625 <prof.anraza3@gmail.com>
2026-04-15 06:38:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 user_question = st.text_input("Ask me a question!")
 if st.button("Submit"):
+    st.write("You asked: ", user_question)
     st.title("SMN_AI🤖")
     st.write(
         "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
